5b3315e25e483babdfad750e8d3e9d77b444ffbd
Jakub Plecháč <loxtox74@gmail.com>
2021-01-26 13:16:49 +0100

    made more components

---
+++ frontend/src/components/HomePage/Copyright.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Copyright = () => {
+  return (
+    <div>
+      <div class="footer-copyright py-3">
+        © 2019 Copyright:
+        <a href="https://mdbootstrap.com/education/bootstrap/" target="_blank">
+          {" "}
+          MDBootstrap.com{" "}
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Copyright;
