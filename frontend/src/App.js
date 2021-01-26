fd6f41ef6f1bc396dea5762e20f188d7abffc592
Jakub Plecháč <loxtox74@gmail.com>
2021-01-26 13:44:27 +0100

    moved components to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+import Navbar from "./components/Navbar";
+import MainContent from "./components/MainContent";
+import Copyright from "./components/Copyright";
 import "./App.css";
-import HomePage from "./components/HomePage/HomePage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <MainContent />
+      </main>
+      <footer class="page-footer text-center font-small mt-4 wow fadeIn">
+        <Copyright />
+      </footer>
+    </div>
+  );
 }
 
 export default App;
