8695d0d9fbfaf47ab704cc3508e37cb00a673f1c
Jakub Plecháč <loxtox74@gmail.com>
2021-01-26 23:00:32 +0100

    implemented pagination

--- frontend/src/components/Pagination.js
+++ frontend/src/components/Pagination.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Pagination = () => {
-  return <div>implement material ui pagination</div>;
+  return (
+    <div>
+      <nav aria-label="Page navigation">
+        <ul className="pagination pagination-lg justify-content-center">
+          <li className="page-item ">
+            <Link className="page-link" to="/" aria-disabled="true">
+              Previous
+            </Link>
+          </li>
+          <li className="page-item active">
+            <Link className="page-item page-link" to="/">
+              1
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link" to="/">
+              2
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link" to="/">
+              3
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link" to="/">
+              Next
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Pagination;
