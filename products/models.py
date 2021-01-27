43f95f638db6d1e564fe10eda426bdb3eedc15b8
Jakub Plecháč <loxtox74@gmail.com>
2021-01-27 22:00:31 +0100

    created products objects

--- products/models.py
+++ products/models.py
@@ -17,8 +17,8 @@ class Product(models.Model):
     title = models.CharField(max_length=20)
     category = models.CharField(max_length=2, choices=CATEGORY_CHOICES)
     price = models.IntegerField()
-    description = models.TextField()
-    image = models.ImageField()
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='frontend/images', default='default.jpg')
     label = models.CharField(
         max_length=2, choices=LABEL_CHOICES, blank=True, null=True
     )
