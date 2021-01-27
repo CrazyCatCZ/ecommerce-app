f116e68a3d155e33ad70df067afa8c8897fed88c
Jakub Plecháč <loxtox74@gmail.com>
2021-01-27 11:42:33 +0100

    implemented login and register

--- backend/backend/schema.py
+++ backend/backend/schema.py
@@ -1,15 +1,13 @@
 import graphene
-import graphql_jwt
+from users.schema import UserQuery, UserMutation 
+
 
 class Query(graphene.ObjectType):
     hello = graphene.String(default_value="Hi!")
 
 
-
-class Mutation(graphene.ObjectType):
-    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
-    verify_token = graphql_jwt.Verify.Field()
-    refresh_token = graphql_jwt.Refresh.Field()
+class Mutation(UserQuery, UserMutation, graphene.ObjectType):
+    pass
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
