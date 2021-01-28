6109ade3d4b2507358ab20f3b76463d8d18f0b92
Jakub Plecháč <loxtox74@gmail.com>
2021-01-28 19:13:44 +0100

    created Order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from users.models import User
+from products.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def calculate_final_price(self):
+        return self.quantity * self.product.price
+
+    def __str__(self):
+        return self.quantity + self.product.title
+
