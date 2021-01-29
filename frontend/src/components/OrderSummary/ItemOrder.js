8ac422b5a8d8196b2a97129d25cdd119951ac71b
Jakub Plecháč <loxtox74@gmail.com>
2021-01-29 15:21:24 +0100

    changed fex styles in cart summary component

--- frontend/src/components/OrderSummary/ItemOrder.js
+++ frontend/src/components/OrderSummary/ItemOrder.js
@@ -11,14 +11,14 @@ const ItemOrder = ({ itemNumber, title, price, quantity }) => {
         <td>{title}</td>
         <td>{price}$</td>
         <td>
-          <i className="fas fa-minus mr-2"></i>
+          <i className="fas fa-minus mr-2 counter-icon"></i>
           <span>{quantity}</span>
-          <i className="fas fa-plus ml-2"></i>
+          <i className="fas fa-plus ml-2 counter-icon"></i>
         </td>
         <td>
           {totalItemPrice}$
           <Link style={{ color: "red" }} to="/">
-            <i className="fas fa-trash float-right"></i>
+            <i className="fas fa-trash float-right fa-lg"></i>
           </Link>
         </td>
       </tr>
