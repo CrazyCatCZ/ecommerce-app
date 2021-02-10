d9649587804c69cdbac0405232c9fc423315d4a5
Jakub Plecháč <loxtox74@gmail.com>
2021-02-10 22:08:17 +0100

    created products.json file

--- users/models.py
+++ users/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     session_id = models.CharField(unique=True, max_length=100)
 
     def __str__(self):
-        if self.user:
-            return self.user.username
-        else:
-            return self.session_id
+        return self.session_id
+
+
+class CustomUser(AbstractUser):
+    customer = models.ForeignKey(
+        Customer, null=True, blank=True, on_delete=models.SET_NULL
+    )
