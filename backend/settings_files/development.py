fc19609b7ceb9585d14bc3ac825f22252cb0fbfd
Jakub Plecháč <loxtox74@gmail.com>
2021-02-13 16:49:36 +0100

    changed cors whitelist

--- backend/settings_files/development.py
+++ backend/settings_files/development.py
@@ -13,7 +13,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:3000',
-    'https://next-amazon.com',
+    'https://next-amazon.herokuapp.com',
 ]
 
 # Whitenoise
